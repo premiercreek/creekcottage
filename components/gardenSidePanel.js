bc1fd1cbbe779e06f01d48094c1de820a2398b49
pkail <pkail@runbox.com>
2021-12-18 17:00:09 -0800

    Sorted out formatting issues

--- components/gardenSidePanel.js
+++ components/gardenSidePanel.js
@@ -8,7 +8,6 @@ const router = useRouter();
 	return (
 		<div className="sidePanel">
 		
-<div className="sidePanelHotelText">
 	<h1>The Gardens</h1>
 	<h3>The Creek</h3>
 	<p>Creek Cottage is named after a creek which runs up one side of the property.  The water in the creek is drinkable, and makes a refreshing change from the mains water in Queen Charlotte, which is heavily chlorinated.</p><br/>
@@ -18,7 +17,6 @@ const router = useRouter();
 	<p> While you are in the garden you may spot two of Haida Gwaii's definitive species, the common raven (Corvus corax) and the bald eagle (Haliaeetus leucocephalus).  These two animals are so intertwined in Haida culture that the Haida people are divided into two clans, 'the ravens' and 'the eagles'.  The raven is a key player in the Haida creation myth, and both birds feature heavily on Haida totem poles.</p>
 </div>
 
-		</div>
 	);
 };
 
